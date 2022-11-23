937ea3e35382d411cba04762ddaa94d4bd186040
mainigautam <gautammaini9@gmail.com>
2022-11-23 18:38:22 +0530

    Init Backend Repo

---
+++ db/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+  username:{
+    type:String,
+    required: [true,"Please enter a useranme"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+// export UserSchema
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
